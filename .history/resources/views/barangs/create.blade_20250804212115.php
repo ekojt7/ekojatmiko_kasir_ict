7e8c61c08acefa83419a803d34e21b91f5751148
ekojt7 <ekojt7@gmail.com>
2025-08-04 21:25:37 +0700

    Menambahkan Validasi Pada Modul Barang

--- .history/resources/views/barangs/create.blade_20250804212115.php
+++ .history/resources/views/barangs/create.blade_20250804212115.php
@@ -1,9 +1,9 @@
 @extends('layout')
 
 @section('content')
-<h2>Edit Barang</h2>
+<h2>Tambah Barang</h2>
 
-@if($errors->any())
+@if ($errors->any())
     <div class="alert alert-danger">
         <ul class="mb-0">
             @foreach ($errors->all() as $error)
@@ -13,26 +13,22 @@
     </div>
 @endif
 
-<form action="{{ route('barangs.update', $barang->id) }}" method="POST">
+
+<form action="{{ route('barangs.store') }}" method="POST">
     @csrf
-    @method('PUT')
-    
     <div class="mb-3">
         <label for="kode_barang" class="form-label">Kode Barang</label>
-        <input type="text" class="form-control" name="kode_barang" value="{{ old('kode_barang', $barang->kode_barang) }}" readonly>
+        <input type="text" class="form-control" name="kode_barang" value="{{ old('kode_barang') }}" disabled>
     </div>
-    
     <div class="mb-3">
         <label for="nama_barang" class="form-label">Nama Barang</label>
-        <input type="text" class="form-control" name="nama_barang" value="{{ old('nama_barang', $barang->nama_barang) }}" required>
+        <input type="text" class="form-control" name="nama_barang" value="{{ old('nama_barang') }}">
     </div>
-
     <div class="mb-3">
         <label for="harga" class="form-label">Harga</label>
-        <input type="number" class="form-control" name="harga" value="{{ old('harga', $barang->harga) }}" min="0" required>
+        <input type="number" class="form-control" name="harga" value="{{ old('harga') }}">
     </div>
-
-    <button type="submit" class="btn btn-primary">Update</button>
+    <button type="submit" class="btn btn-success">Simpan</button>
     <a href="{{ route('barangs.index') }}" class="btn btn-secondary">Kembali</a>
 </form>
 @endsection
